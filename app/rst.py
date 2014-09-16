406dbdb1192b20a0f79106f870567738003e3b99
Ceasar Bautista <cbautista2010@gmail.com>
2014-09-15 20:13:02 -0700

    Generate templates dynamically
    This patch makes it possible to generate HTML from rST programmatically
    (i.e. in Python), which eliminates the need to compile templates from
    the command line.
    
    Closes #6.

--- app/rst.py
+++ app/rst.py
@@ -1,5 +1,11 @@
+"""
+Utility functions for working with reStructuredText.
+"""
 import os
 import os.path
+
+from docutils import core
+from docutils.writers.html4css1 import Writer, HTMLTranslator
 
 
 def iteritems(filename):
@@ -24,3 +30,34 @@ def get_hyperlink_target(index, reference_name):
         return os.path.splitext(hyperlink_target)[0]
     else:
         return hyperlink_target
+
+
+def rst_to_html(rst_string, settings=None):
+    """Convert a string written in reStructuredText to an HTML string.
+
+    :param rst_string:
+        A string that holds the contents of a reStructuredText document.
+
+    :param settings:
+        Optional. A dictionary which overrides the default settings.
+
+        For a list of the possible keys and values, see
+        http://docutils.sourceforge.net/docs/user/config.html.
+
+        To log the values used, pass in ``'dump_settings': 'yes'``.
+    """
+    writer = Writer()
+    writer.translator_class = HTMLTranslator
+    if settings is None:
+        settings = {
+            # Include a time/datestamp in the document footer.
+            'datestamp': '%Y-%m-%d %H:%M UTC',
+            # Recognize and link to standalone PEP references (like "PEP 258").
+            'pep_references': 1,
+            # Do not report any system messages.
+            'report_level': 'none',
+            # Recognize and link to standalone RFC references (like "RFC 822").
+            'rfc_references': 1,
+        }
+    return core.publish_string(rst_string, writer=writer,
+                               settings_overrides=settings)
---
+++ app/settings.py
@@ -0,0 +1,8 @@
+INDEX = "config/index.rst"
+INDEX_PATH = '.index'
+
+# Path to directives the user wants to include
+DIRECTIVES = "config/directives.rst"
+
+# Directory contain rst source files
+SRC = "src"

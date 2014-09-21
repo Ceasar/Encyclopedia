c9d5caa7371ad0f3b01058d4b99d7e1c91c707b8
Ceasar Bautista <cbautista2010@gmail.com>
2014-09-21 10:46:28 -0700

    Test Document.datetime

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,13 @@
+import datetime
+
 from app.models import Document
 
 
 def test_title():
     doc = Document("Test")
     assert doc.title == "Test"
+
+
+def test_datetime():
+    doc = Document("Test", time=1411320556.0)
+    assert doc.datetime == datetime.datetime(2014, 9, 21, 10, 29, 16)

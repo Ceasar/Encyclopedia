6e6e4e045db28446aa0d1013495039d4fb8cb29b
Ceasar Bautista <cbautista2010@gmail.com>
2014-10-03 14:56:30 -0700

    Set up Travis CI integration

--- tests/test_models.py
+++ tests/test_models.py
@@ -10,7 +10,7 @@ def test_title():
 
 def test_datetime():
     doc = Document("Test", time=1411320556.0)
-    assert doc.datetime == datetime.datetime(2014, 9, 21, 10, 29, 16)
+    assert doc.datetime == datetime.datetime(2014, 9, 21, 17, 29, 16)
 
 
 def test_filename():

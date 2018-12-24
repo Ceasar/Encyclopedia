d18e5e03e8a022579bb078c14fdaa80de493d3ee
Ceasar <cbautista2010@gmail.com>
2018-12-24 14:46:57 -0500

    Rename app to application

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,8 @@ import os
 from app import create_app
 
 
-app = create_app(os.environ['ACCESS_TOKEN'])
+application = create_app(os.environ['ACCESS_TOKEN'])
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    application.run(debug=True, port=5001)

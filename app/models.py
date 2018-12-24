5d6a54043210a6a0b2df1cc3c6ce7625b10530cd
Ceasar <cbautista2010@gmail.com>
2018-12-23 23:29:25 -0500

    Remove app.models

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from whoosh.fields import Schema, ID, KEYWORD, STORED, TEXT
-
-SCHEMA = Schema(
-    content=TEXT(stored=True),
-    tags=KEYWORD,
-    title=ID(unique=True, stored=True),
-    time=STORED,
-)
